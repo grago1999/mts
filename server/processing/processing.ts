412ffe4672721a8c81798825db1aa62e82ca620f
Gianluca Rago <grago1999@gmail.com>
2021-02-20 11:35:40 -0500

    uncomment

--- server/processing/processing.ts
+++ server/processing/processing.ts
@@ -61,58 +61,58 @@ function isSimilar(first : string, second : string, sharedLetterThreshold : numb
 
 
 // big function that takes in all the input and slots them into groups
-// export function returnGroups(input : string[]) : {[name : string] : number} {
-//   var out : {[name : string] : number} = {};
-//   var stringGroups : string[][] = [];
-//   var tokenizer = new natural.WordTokenizer();
-//   for (let str of input) {
-//     str = str.toLowerCase();
-//     // we don't want to completely ignore whitespace, but we want the
-//     // whitespace to be consistent, so we split the string and then rejoin it
-//     str = tokenizer.tokenize(str).join(" ");
-//     if (typeof(out[str]) === 'undefined') {
-//       out[str] = 1;
-//       // note that this is a little arbitrary/greedy in terms of grouping words
-//       // together
-//       var strGroup : string[] = [];
-//       var found = false;
-//       for (let group in stringGroups) {
-//         for (let word in group) {
-//           if (isSimilar(str, word)) {
-//             found = true;
-//             strGroup = group;
-//             break;
-//           }
-//         }
-//         if (found) {
-//           break;
-//         }
-//       }
-//       strGroup.push(str);
-//       if (!found) {
-//         stringGroups.push(strGroup);
-//       }
-//     } else {
-//       out[str] = out[str] + 1;
-//     }
-//   }
-//   var realOut : {[name : string] : number} = {};
-//   for (let strGroup in stringGroups) {
-//     var bestStr = "";
-//     var bestScore = 0;
-//     var total = 0;
-//     for (let str in strGroup) {
-//       total = out[str] + total;
-//       if (out[str] > bestScore) {
-//         bestScore = out[str];
-//         bestStr = str;
-//       }
-//     }
-//     realOut[bestStr] = total;
-//   }
+export function returnGroups(input : string[]) : {[name : string] : number} {
+  var out : {[name : string] : number} = {};
+  var stringGroups : string[][] = [];
+  var tokenizer = new natural.WordTokenizer();
+  for (let str of input) {
+    str = str.toLowerCase();
+    // we don't want to completely ignore whitespace, but we want the
+    // whitespace to be consistent, so we split the string and then rejoin it
+    str = tokenizer.tokenize(str).join(" ");
+    if (typeof(out[str]) === 'undefined') {
+      out[str] = 1;
+      // note that this is a little arbitrary/greedy in terms of grouping words
+      // together
+      var strGroup : string[] = [];
+      var found = false;
+      for (let group in stringGroups) {
+        for (let word in group) {
+          if (isSimilar(str, word)) {
+            found = true;
+            strGroup = group;
+            break;
+          }
+        }
+        if (found) {
+          break;
+        }
+      }
+      strGroup.push(str);
+      if (!found) {
+        stringGroups.push(strGroup);
+      }
+    } else {
+      out[str] = out[str] + 1;
+    }
+  }
+  var realOut : {[name : string] : number} = {};
+  for (let strGroup in stringGroups) {
+    var bestStr = "";
+    var bestScore = 0;
+    var total = 0;
+    for (let str in strGroup) {
+      total = out[str] + total;
+      if (out[str] > bestScore) {
+        bestScore = out[str];
+        bestStr = str;
+      }
+    }
+    realOut[bestStr] = total;
+  }
 
-//   return realOut;
-// }
+  return realOut;
+}
 
 // console.log(isSimilar("csgo", "csgi") === true ? "success" : "fail")
 // console.log(isSimilar("csgo", "cshd") === false ? "success" : "fail")
